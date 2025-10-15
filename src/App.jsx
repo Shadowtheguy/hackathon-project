e395c1b481763c5abc25aa27e0c712067e399336
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-10-14 21:44:04 -0500

    Other things

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+// import Home from './pages/Home'
+// import Personal from './pages/Personal'
+// import Top50 from './pages/Top50'
 
 function App() {
 
   return (
-    <></>
+    <>
+    <section className='container'>
+      <h1>Welcome to website weeeee</h1>
+    </section>
+    <section className='container'>
+      <nav>
+        <button>option1</button>
+        <button>option2</button>
+        <button>option3</button>
+      </nav>
+    </section>
+    <section className='container'>
+      <h2 className='titles'>title</h2>
+      <p className='descriptors'>descriptino BL:ALKABFKASDFIASHG POIJAAGHUOISD HGOISHGIHS GOIHSIOGH SHSDGIOH SDOIGHSOIDHGOISDH GOISHDGOIS HGOISHDGOIHS GOIHSGOI HSIOGHSOIGH OSIHGOISHGDOIS</p>
+    </section>
+    </>
   )
 }
 
