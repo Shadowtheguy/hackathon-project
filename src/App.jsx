9ea496dece919a2f35272b64ee3e5e88d88336b4
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-10-17 21:41:17 -0500

    Added Opinion list, updated Personal Page to show some of the list, and filter lists to what's needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import data from "./data/data.json"
+import opinion from "./data/opinion.json"
 import Top50 from "./pages/Top50.jsx"
 import Personal from "./pages/Personal.jsx"
 import Home from './pages/Home.jsx'
